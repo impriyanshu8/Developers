e2fe283ecf335e7ca735a4dc457a93b64d27377b
Yakash goyal <yakashgoyal123@gmail.com>
2025-06-22 13:03:57 +0530

    sonner fixed

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,10 +1,14 @@
 
 import Header from '@/components/Header';
+import Hero from '@/components/Hero';
+import HomepageContent from '@/components/HomepageContent';
 
 const Index = () => {
   return (
     <div className="min-h-screen">
       <Header />
+      <Hero />
+      <HomepageContent />
     </div>
   );
 };
