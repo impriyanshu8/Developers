0012a6736fa1f64a82469311235540d2b53d0194
Yakash goyal <yakashgoyal123@gmail.com>
2025-06-22 12:29:48 +0530

    Tailwind error fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import tailwindcss from '@tailwindcss/vite'
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
@@ -11,6 +12,7 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
+    // tailwindcss(),
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
