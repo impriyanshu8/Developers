35b6a041a32d977f262effb07871cf5f89f9a23f
Shivam-Kharbanda <shivamk290705@gmail.com>
2025-06-23 18:37:01 +0530

    Created AboutPU page and some basic changes

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "@/pages/Index";
 import ContactPage from "@/pages/ContactPage";
+import AboutPU from "@/pages/AboutPU";
 
 
 const queryClient = new QueryClient();
@@ -18,6 +19,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
+          <Route path="/about/pu" element={<AboutPU />} />
           <Route path="/contact" element={<ContactPage />} />
         </Routes>
       </BrowserRouter>
