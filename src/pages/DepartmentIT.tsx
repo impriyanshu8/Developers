dfe498a00387b99860391ad46ab084bc256ea99d
goel-vipul <goelvipul2492005@gmail.com>
2025-06-28 23:38:14 +0530

    updated IT department page content

--- src/pages/DepartmentIT.tsx
+++ src/pages/DepartmentIT.tsx
@@ -12,26 +12,39 @@ import {
 
 const DepartmentIT = () => {
   const programs = [
-    { name: 'B.E. Information Technology', duration: '4 Years', intake: '60' },
-    { name: 'M.E. Information Technology', duration: '2 Years', intake: '18' }
+    { name: 'B.E. Information Technology', duration: '4 Years', intake: '120', details: '' },
+    { name: 'M.E. Information Technology', duration: '2 Years', intake: '30', details: 'B.E. or B.Tech. or equivalent degree in Information Technology / Computer Science & Engineering / Computer Engineering/ Electronics & Communication Engineering / Electrical & Electronics Engineering / Computer Science & Electronics Engineering /Computer Science & Business Systems/ Data Science/ Artificial Intelligence/ Artificial Intelligence and Machine Learning/Cyber Security/ Software Engineering/ Electronics and Computer Engineering/ Robotics and Automation, with at least 60% marks in aggregate from Panjab University or any other University recognized by Panjab University as equivalent thereto. GATE qualified candidates are awarded AICTE scholarship of Rs. 12400/- per month. Department provides financial support for attending workshops, conferences and other technical events.' },
+    { name: 'Ph.D. Faculty of Engineering', duration: '5 Years', intake: '15', details: 'Admission on the basis of GATE/UGC-NET/PU Entrance test. Please visit Panjab University Website for information regarding Ph.D. Entrance Test. http://www.puchd.ac.in/' }
   ];
 
   const faculty = [
-    { name: 'Dr. Manish Kumar', position: 'Professor & HOD', specialization: 'Data Mining' },
-    { name: 'Dr. Sonia Sharma', position: 'Associate Professor', specialization: 'Cloud Computing' },
-    { name: 'Dr. Rahul Gupta', position: 'Assistant Professor', specialization: 'Cybersecurity' },
-    { name: 'Dr. Pooja Singh', position: 'Assistant Professor', specialization: 'Machine Learning' }
+    { name: 'Dr. Amandeep Verma', position: 'Co-ordinator and Associate Professor', specialization: 'Cloud Computing', email: 'amandeepverma@pu.ac.in', contact_no: '9781777730' },
+    { name: 'Prof. Krishan Kumar', position: 'Professor', specialization: 'Network Security and Computer Networks', email: 'k.saluja@pu.ac.in', contact_no: '8288012014' },
+    { name: 'Prof. Roopali Garg', position: 'Professor', specialization: 'Wireless Communication', email: 'roopali.garg@pu.ac.in', contact_no: '9417724824' },
+    { name: 'Prof. Inderdeep Kaur Aulakh', position: 'Professor', specialization: 'Wireless Communication Network', email: 'ikaulakh@pu.ac.in', contact_no: '9814788999' },
+    { name: 'Dr. Veenu Mangat', position: 'Professor', specialization: 'Data Mining', email: 'vmangat@pu.ac.in', contact_no: '9814475450' },
+    { name: 'Dr. Pamela Kumar', position: 'Visiting Professor', specialization: '', email: 'pamelakumar@ieee.org', contact_no: '' },
+    { name: 'Dr. Mandeep Kaur', position: 'Associate Professor', specialization: 'Digital Image Processing', email: 'mandeep@pu.ac.in', contact_no: '7973690839' },
+    { name: 'Dr. Puneet Jai Kaur', position: 'Associate Professor', specialization: 'Software Quality Assurance, Aspect Oriented Software Development, Data Mining, Security in Cloud Computing and IoT', email: 'puneet@pu.ac.in', contact_no: '9914257047' },
+    { name: 'Dr. Monika Sharma', position: 'Associate Professor', specialization: ' Software Engineering.', email: 'monikahsp@gmail.com', contact_no: '8968175076' },
+    { name: 'Dr. Sukesha Sharma', position: 'Assistant Professor', specialization: 'Machine learning, Artificial Intelligence, IOT, Embedded Systems, Mechatronics', email: 'sukesha@pu.ac.in', contact_no: '9815472778' },
+    { name: 'Ms. Raj Kumari', position: 'Assistant Professor', specialization: ' Parallel & Distributed Computing', email: 'rajkumari_bhatia5@yahoo.com', contact_no: '9417917607' },
+    { name: 'Dr. Neelam Goel', position: 'Assistant Professor', specialization: ' Bioinformatics, Soft Computing', email: 'neelam.goyal85@gmail.com', contact_no: '9468427952' },
+    { name: 'Dr. Yogita', position: 'Assistant Professor', specialization: ' Data Mining, Soft Computing', email: 'thakranyogita@gmail.com', contact_no: '9646837469' },
+    { name: 'Ms. Rajni Sobti', position: 'Assistant Professor', specialization: 'Speech Recognition', email: 'sobtirajni@yahoo.co.in', contact_no: '9417433175' },
+    { name: 'Dr. Sukhvir Singh', position: 'Assistant Professor', specialization: ' Wireless Networks', email: 'sukhvirpu@gmail.com', contact_no: '9855804567' },
+    { name: 'Mr. Rajneesh Singla', position: 'Assistant Professor', specialization: 'Image Processing', email: '', contact_no: '' },
+    { name: 'Mr. Arun Dhawan', position: 'Programming Assistant', specialization: '', email: '', contact_no: '' },
+    { name: 'Mr. Vikas Bali', position: 'Programming Assistant', specialization: '', email: '', contact_no: '' },
   ];
 
   const labs = [
-    'Programming Lab',
-    'Database Management Lab',
-    'Network Security Lab',
-    'Cloud Computing Lab',
-    'Mobile Application Lab',
-    'Data Analytics Lab',
-    'IoT Lab',
-    'Project Lab'
+    'Embedded System Lab',
+    'Wireless Communication Lab',
+    'C/C++ Programming Lab Unix / Networking Lab',
+    'Multimedia Lab',
+    'Operating System Lab',
+    'Software Engineering / Databases Lab',
   ];
 
   return (
@@ -74,8 +87,7 @@ const DepartmentIT = () => {
                 </CardHeader>
                 <CardContent>
                   <p className="text-gray-700">
-                    To be a leading department in Information Technology education and research, 
-                    fostering innovation and developing skilled IT professionals for the digital age.
+                    The Department of Information Technology aims to develop information technology engineers who work professionally and creatively for the advancement of technology and betterment of society.
                   </p>
                 </CardContent>
               </Card>
@@ -89,9 +101,14 @@ const DepartmentIT = () => {
                 </CardHeader>
                 <CardContent>
                   <ul className="text-gray-700 space-y-2">
-                    <li>• Provide cutting-edge IT education and training</li>
-                    <li>• Promote research in emerging technologies</li>
-                    <li>• Develop industry-ready IT professionals</li>
+                    <li>• To impart quality education by developing information technology facilities, faculty and resources that generates professionals who are leaders for a dynamic information society.
+                    </li>
+                    <li>• To develop a collaborative culture, so as to nurture an environment of increased research amongst the students and faculty.
+                    </li>
+                    <li>• To encourage hands-on learning by fostering industrial partnerships to create real world solutions through innovation, product development, entrepreneurship and consultancy services.
+                    </li>
+                    <li>• To enhance human potential by encouraging transparency and accountability amongst all stakeholders, in order to nurture ethical values in students.
+                    </li>
                   </ul>
                 </CardContent>
               </Card>
@@ -112,6 +129,8 @@ const DepartmentIT = () => {
                     </CardTitle>
                   </CardHeader>
                   <CardContent>
+                    <p>{program.details}</p>
+                    <br />
                     <p className="text-gray-600">Intake: {program.intake} students</p>
                   </CardContent>
                 </Card>
@@ -126,7 +145,9 @@ const DepartmentIT = () => {
                   <CardContent className="p-6">
                     <h3 className="font-semibold text-lg">{member.name}</h3>
                     <p className="text-green-600 font-medium">{member.position}</p>
-                    <p className="text-gray-600">Specialization: {member.specialization}</p>
+                    <p>Specialization: {member.specialization}</p>
+                    <p className="text-gray-600">Email: {member.email}</p>
+                    <p className="text-gray-600">Contact No.: {member.contact_no}</p>
                   </CardContent>
                 </Card>
               ))}
