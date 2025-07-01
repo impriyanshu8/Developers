63befcec07d0804c6ee5a2c420e6eb0581dc9661
Yakash goyal <yakashgoyal123@gmail.com>
2025-07-01 20:41:09 +0530

    Added faculty page

---
+++ src/pages/FacultyPage.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '@/components/Header';
+import Faculty from '@/components/Faculty';
+import Footer from '@/components/Footer';
+
+const FacultyPage = () => {
+  return (
+    <div className="min-h-screen">
+      <Header />
+      <Faculty />
+      <Footer />
+    </div>
+  );
+};
+
+export default FacultyPage;
